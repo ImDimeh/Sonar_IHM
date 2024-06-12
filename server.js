5467f253e43c0aeb920b08016552cf2e1fe81cf9
MEHDI BELLAM <porcepic91@gmail.com>
2024-06-12 10:18:56 +0200

    delete port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const buffers = require('buffer');
 console.log(ip.address())
 let i = 0
 let color = [0, 0, 0]
-const port = 3000;
+
 
 /*const io = new Server(httpServer, {
     cors : {
@@ -35,13 +35,7 @@ app.post('/pos', (req, res) => {
   console.log("pos")
   console.log(pos)
   // RECREER un chart
-    
+
   //io.emit("Sendpos", pos)
   res.send({ "msg": "ok post" })
-});
-
-
-
-httpServer.listen(port, () => {
-  console.log(`On écoute le port n°${port}`)
 });
